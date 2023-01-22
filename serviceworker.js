4a29d30008a99f5183f54c22d6c5bc3c1d15623b
AndrewWladis <andrewwladis@gmail.com>
2023-01-21 21:07:46 -0500

    tryed to turn this into a PWA

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,34 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/serviceworker.js");
-}
+const assets = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/android-chrome-192x192.png",
+  "/android-chrome-384x384.png",
+  "/apple-touch-icon.png",
+  "/background.svg",
+  "/browserconfig.xml",
+  "/favicon-16x16.png",
+  "/favicon-32x32.png",
+  "/favicon.ico",
+  "/Grid.js",
+  "/mstile-150x150.png",
+  "/safari-pinned-tab.svg",
+  "/Tile.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
